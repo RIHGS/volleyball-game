6908d0e92800692f2f68710bf4599da4a82e9145
RIHGS <grhigsam@gmail.com>
2026-06-18 19:09:29 +0545

    Add files via upload

---
+++ src/constants.hpp
@@ -0,0 +1,14 @@
+
+#pragma once
+
+const int   W = 800;
+const int   H = 600;//window size
+const float GRAVITY = 0.4f;
+const float GROUND_Y = 550.f;
+const float NET_X = 390.f;
+const float NET_TOP = 400.f;
+const float BALL_RADIUS = 15.f;
+const float P_SPEED = 4.f;
+const float P_JUMP = -12.f;//negatixe xa coz upward m otion against gravity 
+const float P_W = 40.f;
+const float P_H = 60.f;
